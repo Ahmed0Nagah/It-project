7d1e10393be17f617d40e7440a7f8d864f6b95f4
Ahmed0Nagah <mido29092005@gmail.com>
2024-04-30 18:27:44 +0300

    it

--- login.js
+++ login.js
@@ -1,5 +1,15 @@
 var btn = document.getElementById('showpass');
+var username = document.getElementById('uname');
 var pass = document.getElementById('pass');
+var butt = document.getElementById('btn').addEventListener('click',function(){
+    var errormessage = document.getElementById('errormessege');
+    if(username.value === ''|| pass.value === ''){
+    errormessage.innerHTML = "wrong username or password!";
+    errormessage.className = 'error';}
+    else{
+        open('home&login/home.html');
+    }
+})
 function showpass(){
 if( pass.type === 'password'){
     pass.type = 'text';
@@ -10,6 +20,7 @@ if( pass.type === 'password'){
     btn.classList.add('fa-eye');
     btn.classList.remove('fa-eye-slash');}
 }
+
 function link(){
     open("home&login/home.html");
 }
